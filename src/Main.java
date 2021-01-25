a2aa7725d61c15a389f8727f11fe78d4552749d8
SantiMartinez <santiago.martinez@iesnervion.es>
2021-01-25 14:10:06 +0100

    Commit 2

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
         //clase persona
         Persona persona1 = new Persona();
-        Persona persona2 = new Persona("Pepe", "Sanchez de Toro", 58, 'm');
+        Persona persona2 = new Persona("Pepe", "Sanchez", 58, 'm');
 
         persona2.saludar();
 
         //clase horas
-        Hora horaPrueba = new Hora(00, 23, 56);
+        Hora horaPrueba = new Hora(0, 23, 56);
         horaPrueba.sumaSegundos(6);
     }
 
